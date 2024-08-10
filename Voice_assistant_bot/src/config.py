e17a26907425c9d24ba769e5ecb0b3606ce2af20
smcftp <156796937+smcftp@users.noreply.github.com>
2024-08-10 13:19:33 +0300

    Update config.py

--- Voice_assistant_bot/src/config.py
+++ Voice_assistant_bot/src/config.py
@@ -8,6 +8,7 @@ from openai import OpenAI
 
 from concurrent.futures import ThreadPoolExecutor
 
+
 class Settings(BaseSettings):
     openai_api_key: str
     telegram_bot_token: str
@@ -15,7 +16,7 @@ class Settings(BaseSettings):
     amplitude_api_key: str
 
     class Config:
-        env_file = 'Voice_assistant_bot_2\.env'
+        env_file = 'D:\Programming\Python\GPT\Voice_AI_bot_on_Aiogram\Voice_assistant_bot_2\.env'
 
 set = Settings()
 
@@ -84,11 +85,11 @@ assistant = client.beta.assistants.update(
 
 # Upload the user provided file to OpenAI
 message_file_0 = client.files.create(
-    file = open("At_Latoken.docx", "rb"), purpose="assistants"
+    file = open("C:\\Users\\thatn\\Desktop\\At_Latoken.docx", "rb"), purpose="assistants"
 )
 
 message_file_1 = client.files.create(
-    file = open("Anxiety.docx", "rb"), purpose="assistants"
+    file = open("C:\\Users\\thatn\\Desktop\\Anxiety.docx", "rb"), purpose="assistants"
 )
     
 # Create a thread and attach the file to the message
